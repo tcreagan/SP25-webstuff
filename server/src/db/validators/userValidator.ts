00d85ad12dc1657416312ac6177bb14aa64ae075
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-02-06 00:43:43 -0500

    added login validator

--- server/src/db/validators/userValidator.ts
+++ server/src/db/validators/userValidator.ts
@@ -1,6 +1,14 @@
+//gpt generated
 import Joi from 'joi';
-
+//user register validation
 export const registerSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+//gpt generated 
+//user login validator
+export const loginSchema = Joi.object({
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
 });
