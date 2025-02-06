116537a015c079240b28f05b2700208d11e8f4df
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-02-06 00:27:00 -0500

    applying authMiddleware to route

--- server/src/routers/authRoutes.ts
+++ server/src/routers/authRoutes.ts
@@ -3,6 +3,8 @@
 //used to create authentication API for login and register
 import { Router } from 'express';
 import { registerUserHandler, loginUserHandler } from '../controllers/authController';
+import { authenticateJWT } from './middlewares/authMiddleware';
+import { getUserProfile } from './controllers/userController';
 
 const router = Router();
 
@@ -11,5 +13,8 @@ router.post('/register', registerUserHandler);
 
 // Route to log in a user
 router.post('/login', loginUserHandler);
+
+// Example of a protected route (user profile)
+router.get('/profile', authenticateJWT, getUserProfile);
 
 export default router;
