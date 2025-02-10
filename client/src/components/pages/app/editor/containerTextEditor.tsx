9ee08661b4247a17e7cbc1dccfd91f94d84cfcec
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-02-10 01:01:22 -0500

    Create containerTextEditor.tsx w/ multiple containers that are independent

---
+++ client/src/components/pages/app/editor/containerTextEditor.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import ResizableContainer from './ResizableContainer';
+import DraggableTextBlock from './DraggableTextBlock';
+import DraggableImageWidget from './DraggableImageWidget';
+
+const ContainerEditor: React.FC = () => {
+  return (
+    <div className="editor">
+      {/* Container 1 */}
+      <ResizableContainer initialWidth={400} initialHeight={300}>
+        <DraggableTextBlock id="text1" initialText="Text Block 1" />
+        <DraggableImageWidget id="image1" initialUrl="https://example.com/image.jpg" />
+      </ResizableContainer>
+
+      {/* Container 2 */}
+      <ResizableContainer initialWidth={600} initialHeight={400}>
+        <DraggableTextBlock id="text2" initialText="Text Block 2" />
+        <DraggableImageWidget id="image2" initialUrl="https://example.com/image2.jpg" />
+      </ResizableContainer>
+    </div>
+  );
+};
+
+export default ContainerEditor;
