439f2881263fc61c77195825076123da0e0afdb3
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-02-12 03:18:48 -0500

    added textblock import and children to one of the cardwidget for testing

--- client/src/components/pages/app/editor/cardEditor.tsx
+++ client/src/components/pages/app/editor/cardEditor.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import CardWidget from 'components/pages/app/widgets/CardWidget';
 // import DraggableTextBlock from './DraggableTextBlock'; //DNE, need to create 
 import DraggableImageWidget from 'components/pages/app/widgets/DraggableImageWidget';
+import DraggableTextBlock from 'components/pages/app/Textbox';
 
 const CardEditor: React.FC = () => {
   return (
     <div className="editor">
       {/* Card 1 */}
-      <CardWidget title="Card 1" initialWidth={300} initialHeight={200}>
+      <CardWidget title="Card 1" initialWidth={300} initialHeight={200} children>
         <DraggableTextBlock id="text1" initialText="Text Block in Card 1" />
       </CardWidget>
 
