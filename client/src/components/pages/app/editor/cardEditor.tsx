b7fab78ad29c673f8dbb6f493f3ae0d401150b32
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-02-12 15:35:05 -0500

    changed draggableTextblock to textbox

--- client/src/components/pages/app/editor/cardEditor.tsx
+++ client/src/components/pages/app/editor/cardEditor.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import CardWidget from 'components/pages/app/widgets/CardWidget';
 // import DraggableTextBlock from './DraggableTextBlock'; //DNE, need to create 
 import DraggableImageWidget from 'components/pages/app/widgets/DraggableImageWidget';
-import DraggableTextBlock from 'components/pages/app/Textbox';
+import Textbox from 'components/pages/app/Textbox';
 
 const CardEditor: React.FC = () => {
   return (
     <div className="editor">
       {/* Card 1 */}
       <CardWidget title="Card 1" initialWidth={300} initialHeight={200} children>
-        <DraggableTextBlock id="text1" initialText="Text Block in Card 1" />
+        <Textbox id="text1" initialText="Text Box in Card 1" />
       </CardWidget>
 
       {/* Card 2 */}
       <CardWidget title="Card 2" initialWidth={500} initialHeight={300}>
         <DraggableImageWidget id="image1" initialUrl="https://example.com/image.jpg" />
-        <DraggableTextBlock id="text2" initialText="Text Block in Card 2" />
+        <Textbox id="text2" initialText="Text Box in Card 2" />
       </CardWidget>
     </div>
   );
