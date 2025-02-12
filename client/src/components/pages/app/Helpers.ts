4cbd2356e6c7016b5b91c7f6820c4887b2e0405d
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-02-11 22:49:48 -0500

    updated widgetId parameter type

--- client/src/components/pages/app/Helpers.ts
+++ client/src/components/pages/app/Helpers.ts
@@ -109,7 +109,7 @@ export function sanitizeImageUrl(value: string, defaultValue: string) {
 //review
 //used to help handle child widgets update their own styles, positions, etc
 //fix the any types in parameters
-export function updateWidgetStyle(widgetId, styles, dispatch) {
+export function updateWidgetStyle(widgetId: number, styles, dispatch) {
   dispatch({
     type: 'UPDATE_WIDGET_STYLE',
     widgetId,
@@ -117,7 +117,7 @@ export function updateWidgetStyle(widgetId, styles, dispatch) {
   });
 }
 
-export function updateWidgetPosition(widgetId, x, y, dispatch) {
+export function updateWidgetPosition(widgetId: number, x, y, dispatch) {
   dispatch({
     type: 'UPDATE_WIDGET_POSITION',
     widgetId,
