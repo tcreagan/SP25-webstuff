ebb5c9999686464e80e102d573d8377a6c2784f6
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-02-12 22:32:51 -0500

    changed initialText to content

--- client/src/components/pages/app/editor/textBlockEditor.tsx
+++ client/src/components/pages/app/editor/textBlockEditor.tsx
@@ -38,7 +38,7 @@ const Editor: React.FC = () => {
       {widgets.map((widget) => (
         <div key={widget.id} style={{ position: 'absolute', left: widget.left, top: widget.top }}>
           <DraggableTextBlock
-            initialText="Editable Text Block"
+            content="Editable Text Block"
             initialStyles={{ width: '200px', height: 'auto', border: '1px solid #ccc' }}
             onSave={(data: any) => console.log('Widget data saved:', data)} //figure out what type to assign to data, attempting to fix using any
           />
