a431df48ff58d0ef1c696334116b72ebd9acabe8
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-02-15 20:10:02 -0500

    Create react-resizable.d.ts

---
+++ client/src/types/react-resizable.d.ts
@@ -0,0 +1,13 @@
+//used to make react-resizable recognizable in the project
+declare module 'react-resizable' {
+  import { Component } from 'react';
+
+  export interface ResizableBoxProps {
+    width: number;
+    height: number;
+    onResizeStop?: () => void;
+    [key: string]: any;  // Add more specific props as needed
+  }
+
+  export class ResizableBox extends Component<ResizableBoxProps> {}
+}
