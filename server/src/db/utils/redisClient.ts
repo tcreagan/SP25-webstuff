58ae9482bef3aa98006a66b8c19aa65a1c0301b7
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-03 17:03:58 -0500

    added socket to redisClient

--- server/src/db/utils/redisClient.ts
+++ server/src/db/utils/redisClient.ts
@@ -1,7 +1,14 @@
 // redisClient needed for blacklisting tokens
 import { createClient } from 'redis';
+import dotenv from 'dotenv'
+dotenv.config();
 
-const redisClient = createClient();
+const redisClient = createClient({
+    socket: {
+      host: process.env.REDIS_HOST || '127.0.0.1',  // Redis host
+      port: 6379,  // Redis port (default is 6379)
+    }
+  });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
