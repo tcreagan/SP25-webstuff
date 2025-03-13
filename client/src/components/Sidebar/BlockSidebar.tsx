3001749bb68f4e21e65437cc370062d8e1200a57
gwengentry <gwengentry@proton.me>
2025-03-12 20:03:22 -0400

    Revert "Removed redundant and obsolete widgets"
    This reverts commit ca2107290659683af11dbfbe9d404c13ee8820cb.

--- client/src/components/Sidebar/BlockSidebar.tsx
+++ client/src/components/Sidebar/BlockSidebar.tsx
@@ -1,10 +1,13 @@
 //thea
 import React, { useEffect } from 'react'
+import verticalContainer from "widgetFiles/verticalContainer.json"
 import twoCol from "widgetFiles/twoCol.json"
 import threeCol from "widgetFiles/threeCol.json"
 import horizontalContainer from "widgetFiles/horizontalContainer.json"
 import textbox from "widgetFiles/textbox.json"
-import heading1 from "widgetFiles/heading.json"
+import heading1 from "widgetFiles/heading1.json"
+import heading2 from "widgetFiles/heading2.json"
+import heading3 from "widgetFiles/heading3.json"
 import image from "widgetFiles/image.json"
 import link from "widgetFiles/link.json"
 import logo from "widgetFiles/logo.json"
@@ -19,11 +22,14 @@ type Props = {};
 const BlockSidebar = (props: Props) => {
   const { state: editor, dispatch: editorDispatch } = useEditor();
 
+  const verticalD: HtmlObject = verticalContainer as HtmlObject;
   const horizontalD: HtmlObject = horizontalContainer as HtmlObject;
   const twoColD: HtmlObject = twoCol as HtmlObject;
   const threeColD: HtmlObject = threeCol as HtmlObject;
   const textboxD: HtmlObject = textbox as HtmlObject;
   const heading1D: HtmlObject = heading1 as HtmlObject;
+  const heading2D: HtmlObject = heading2 as HtmlObject;
+  const heading3D: HtmlObject = heading3 as HtmlObject;
   const imageD: HtmlObject = image as HtmlObject;
   const linkD: HtmlObject = link as HtmlObject;
   const logoD: HtmlObject = logo as HtmlObject;
@@ -36,8 +42,8 @@ const BlockSidebar = (props: Props) => {
     if (editor.widgets.length === 0) {
       editorDispatch({
         type: ActionType.FETCHED_WIDGETS,
-        widgets: [horizontalD,  twoColD, threeColD, textboxD,
-           heading1D, imageD, logoD, nav, videoD, audioD, linkD],
+        widgets: [verticalD, horizontalD,  twoColD, threeColD, textboxD,
+           heading1D, heading2D, heading3D, imageD, logoD, nav, videoD, audioD, linkD],
       });
     }
   });
