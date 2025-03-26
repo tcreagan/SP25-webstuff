75d973fe8a678d19b1e798dad2a5399cfa0915f0
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-25 22:43:37 -0400

    simplified textbox

--- client/src/components/pages/app/TextBox.tsx
+++ client/src/components/pages/app/TextBox.tsx
@@ -1,5 +1,5 @@
 // TextBox.tsx
-
+import React from 'react';
 import { CSSProperties } from 'react';
 
 const TextBoxStyle: CSSProperties = {
@@ -12,66 +12,17 @@ const TextBoxStyle: CSSProperties = {
   width: '100%',
   textAlign: 'center',
 };
-/* old props
+
 interface TextBoxProps {
   content: string;  
-} */
-/*
+}
+
 const TextBox: React.FC<TextBoxProps> = ({ content }) => {
   return (
     <div style={TextBoxStyle}>
       {content}
     </div>
   );
-};*/
-//gpt generated 
-//needs review 
-//text box widget w/ save functionality
-
-import React, { useState } from 'react';
-
-interface TextBoxProps {
-  content: string;
-  onSave: (content: string) => void;
-}
-
-const TextBox: React.FC<TextBoxProps> = ({ content, onSave }) => {
-  const [isEditing, setIsEditing] = useState(false);
-  const [text, setText] = useState(content);
-
-  // Handle text change
-  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setText(event.target.value);
-  };
-
-  // Handle save
-  const handleSave = () => {
-    setIsEditing(false);
-    onSave(text);  // Save the edited text
-  };
-
-  // Enable editing mode
-  const handleEdit = () => {
-    setIsEditing(true);
-  };
-
-  return (
-    <div className="text-box">
-      {isEditing ? (
-        <div>
-          <textarea value={text} onChange={handleChange} />
-          <button onClick={handleSave}>Save</button>
-        </div>
-      ) : (
-        <div onClick={handleEdit}>
-          <p>{text}</p>
-          <button>Edit</button>
-        </div>
-      )}
-    </div>
-  );
 };
-
-
 
 export default TextBox;
