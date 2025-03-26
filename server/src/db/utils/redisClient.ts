a0c17133bde67715023242235bd4edba03883b9b
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-26 01:27:36 -0400

    Delete server/src/db/utils/redisClient.ts

--- server/src/db/utils/redisClient.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from 'redis';
-
-const redisClient = createClient();
-
-redisClient.on('error', (err) => console.error('Redis Client Error', err));
-
-redisClient.connect();
-
-export default redisClient;
