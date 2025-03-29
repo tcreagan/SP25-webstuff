ee207dc132a1700b34974f9fa5816efdc16cbf0f
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-29 00:04:38 -0400

    Update BlockSidebar.tsx

--- client/src/components/Sidebar/BlockSidebar.tsx
+++ client/src/components/Sidebar/BlockSidebar.tsx
@@ -1,16 +1,13 @@
 //thea
 import React, { useEffect } from 'react'
-import verticalContainer from "widgetFiles/verticalContainer.json"
 import twoCol from "widgetFiles/twoCol.json"
 import threeCol from "widgetFiles/threeCol.json"
-import horizontalContainer from "widgetFiles/horizontalContainer.json"
+import horizontalContainer from "widgetFiles/container.json"
 import textbox from "widgetFiles/textbox.json"
-import heading1 from "widgetFiles/heading1.json"
-import heading2 from "widgetFiles/heading2.json"
-import heading3 from "widgetFiles/heading3.json"
-//import image from "widgetFiles/image.json"
-import link from "widgetFiles/link.json"
+import heading from "widgetFiles/heading.json"
+import image from "widgetFiles/image.json"
 import logo from "widgetFiles/logo.json"
+import navigation from "widgetFiles/navigation.json"
 import video from "widgetFiles/video.json"
 import audio from "widgetFiles/audio.json"
 import { HtmlObject } from "types/HtmlObject";
@@ -21,17 +18,14 @@ type Props = {};
 const BlockSidebar = (props: Props) => {
   const { state: editor, dispatch: editorDispatch } = useEditor();
 
-  const verticalD: HtmlObject = verticalContainer as HtmlObject;
   const horizontalD: HtmlObject = horizontalContainer as HtmlObject;
   const twoColD: HtmlObject = twoCol as HtmlObject;
   const threeColD: HtmlObject = threeCol as HtmlObject;
   const textboxD: HtmlObject = textbox as HtmlObject;
-  const heading1D: HtmlObject = heading1 as HtmlObject;
-  const heading2D: HtmlObject = heading2 as HtmlObject;
-  const heading3D: HtmlObject = heading3 as HtmlObject;
-  //const imageD: HtmlObject = image as HtmlObject;
-  const linkD: HtmlObject = link as HtmlObject;
+  const heading1D: HtmlObject = heading as HtmlObject;
+  const imageD: HtmlObject = image as HtmlObject;
   const logoD: HtmlObject = logo as HtmlObject;
+  const nav: HtmlObject = navigation as HtmlObject;
   const videoD: HtmlObject = video as HtmlObject;
   const audioD: HtmlObject = audio as HtmlObject;
 
@@ -40,8 +34,8 @@ const BlockSidebar = (props: Props) => {
     if (editor.widgets.length === 0) {
       editorDispatch({
         type: ActionType.FETCHED_WIDGETS,
-        widgets: [verticalD, horizontalD,  twoColD, threeColD, textboxD,
-           heading1D, heading2D, heading3D, /*imageD,*/ logoD, videoD, audioD, linkD],
+        widgets: [horizontalD,  twoColD, threeColD, textboxD,
+           heading1D, imageD, logoD, nav, videoD, audioD],
       });
     }
   });
