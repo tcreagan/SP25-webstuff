ed1f0e0507be148f2553fbef8977c4667e5a8b2e
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-29 00:07:38 -0400

    Update TemplateSidebar.tsx

--- client/src/components/Sidebar/TemplateSidebar.tsx
+++ client/src/components/Sidebar/TemplateSidebar.tsx
@@ -1,14 +1,64 @@
 //thea
 import React from 'react'
+import templateone from "../../templates/templateone.json"
+import { useSaveLoadActions } from "../../state/editor/Helpers";
+
 
 type Props = {}
 
+type PropsTemplate = {
+  img: string;
+}
+
+///Leaving this here in case i need to revert to it
+/*class Template extends React.Component<PropsTemplate>{
+  image: string;
+  constructor(props: PropsTemplate){
+    super(props);
+    this.image = props.img
+  }
+
+
+  render() {
+    return (
+      <div>
+         <img src={this.image} />
+      </div>
+    );
+
+}};*/
+
+const Template = ({ img, onClick }: { img: string, onClick: () => void })=>{
+  return (
+    <div>
+       <img src={img} onClick={onClick}/>
+    </div>
+  );
+}
+
+const templateImages = [require('../../assets/images/templates/template1wip.png'),
+  require('../../assets/images/templates/template2wip.png'),
+  require('../../assets/images/templates/template3wip.png')];
+
+const templatearr = [String(templateone)];
+
+
 const TemplateSidebar = (props: Props) => {
+  const { loadFromLocalStorage } = useSaveLoadActions();
+  const handleTemplateLoad = () => {
+    loadFromLocalStorage();
+    console.log("Click recieved")
+  }
   return (
     <aside className='style-sidebar'>
        <header className='sidebar-header'>
         <h2>TEMPLATE</h2>
       </header>
+      <div className="grid-container">
+      {templateImages.map((img: string, index: number) => (
+          <Template key={index} img={img} onClick={() => handleTemplateLoad()}/>
+        ))}
+      </div>
       </aside>
     
     
