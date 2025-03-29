13be4d18bc0997f2702c02b8b389d12e318ca9b8
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-29 00:10:03 -0400

    Update Header.tsx

--- client/src/components/pages/app/editor/Header.tsx
+++ client/src/components/pages/app/editor/Header.tsx
@@ -1,22 +1,20 @@
 import {HtmlObject} from "types/HtmlObject"
 import {HtmlInterpreter} from "../HtmlInterpreter"
-import { useEditor } from "state/editor/EditorReducer";
+//import {buildPreview} from "./Editor"
 
 type Props = {
   content: HtmlObject;
+
 };
 
 export const Header = (props: Props) => {
-  const { state: editor } = useEditor();
   let content = [...props.content.html.nodes]
   if(props.content.metadata.preview){
   //  content = buildPreview(props.content)
   }
   
   return (
-    <div className="editor-header-container">
-      <div className="editor-toolbar">
-      </div>
+    <div className="editor-header-container" >
       <HtmlInterpreter prefix="h" content={props.content.html.nodes}/>
     </div>
   )
