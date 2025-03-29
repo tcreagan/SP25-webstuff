08e70a4becc4b6f99ffc77e702a63da06c5914a7
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-29 00:08:37 -0400

    Update Body.tsx

--- client/src/components/pages/app/editor/Body.tsx
+++ client/src/components/pages/app/editor/Body.tsx
@@ -1,20 +1,22 @@
 import {HtmlInterpreter} from "../HtmlInterpreter"
 import {HtmlObject} from "types/HtmlObject"
+import { Editor } from "./Editor"
+//import { buildQueries } from "@testing-library/react";
 
 type Props = {
   content: HtmlObject;
 };
 
 export const Body = (props: Props) => {
-  // Get the nodes to render, either from preview or actual content
-  const nodesToRender = props.content.metadata.preview?.obj?.html.nodes || props.content.html.nodes;
-
+  let content = [...props.content.html.nodes]
+  if(props.content.metadata.preview){
+    //content = buildPreview(props.content)
+  }
   return (
     <div className="editor-body-container">
-      <HtmlInterpreter 
-        prefix="b" 
-        content={nodesToRender}
-      />
+      <HtmlInterpreter prefix="b" content={props.content.html.nodes}/>
     </div>
   )
+
+ 
 };
