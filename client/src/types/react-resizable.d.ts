bd275a88b22c2d422292835bdac87ffcbdb90339
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-31 15:13:08 -0400

    added callbackdata and fixed props

--- client/src/types/react-resizable.d.ts
+++ client/src/types/react-resizable.d.ts
@@ -1,11 +1,27 @@
 //used to make react-resizable recognizable in the project
 declare module 'react-resizable' {
-  import { Component } from 'react';
+  import { Component, SyntheticEvent } from 'react';
+
+  export interface ResizeCallbackData {
+    node: HTMLElement;
+    size: {
+      width: number;
+      height: number;
+    };
+    handle: string;
+  }
 
   export interface ResizableBoxProps {
     width: number;
     height: number;
-    onResizeStop?: () => void;
+    onResizeStop?: (e: SyntheticEvent, data: ResizeCallbackData) => void;
+    onResize?: (e: SyntheticEvent, data: ResizeCallbackData) => void;
+    minConstraints?: [number, number];
+    maxConstraints?: [number, number];
+    resizeHandles?: Array<'s' | 'w' | 'e' | 'n' | 'sw' | 'nw' | 'se' | 'ne'>;
+    draggableOpts?: {
+      grid?: [number, number];
+    };
     [key: string]: any;  // Add more specific props as needed
   }
 
