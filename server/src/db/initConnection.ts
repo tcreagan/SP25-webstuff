aedb3446a4c24b6d1876416613228b532f7cff67
tcreagan <159402059+tcreagan@users.noreply.github.com>
2025-03-30 23:25:40 -0400

    unnecessary file for server use

--- server/src/db/initConnection.ts
+++
@@ -1,6 +0,0 @@
-import DBConnector from "../db/dbConnector";
-import { ModelBuilder } from "./models/Models";
-
-export const dbConnector = new DBConnector()
-
-export const Models = ModelBuilder(dbConnector);
